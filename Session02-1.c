1dc3058805fdecf0b644b44ed1ebef16a5e032b1
latp1102 <latp1102@gmail.com>
2024-11-20 02:31:40 +0700

    Update Session02-1.c

--- Session02-1.c
+++ Session02-1.c
@@ -2,16 +2,16 @@
 
 int main(){
 	
-	// Khai bao mot bien kieu int luu tru môt sô nguyen
+	// Khai bao mot bien kieu int luu tru mÃ´t sÃ´ nguyen
 	int age = 18; // Tuoi cua mot nguoi
 	// Khai bao mot bien kieu float luu tru mot so thuc
-	float salary = 8200000; // Luong cua mot nguoi
+	float heigth = 1.70; // Chieu cao cua mot nguoi
 	// Khai bao mot bien kieu double luu tru mot so thuc
 	double pi = 3.14; // Gia tri cua so pi
-	// Khai bao mot  bien kieu char luu tru ký tu
+	// Khai bao mot  bien kieu char luu tru kÄ± tu
 	char grade = 'A'; // Diem so cua hoc sinh
 	// Khai bao mot bien kieu long integer luu tru cac gia tri so ngyen lon nhat
-	long bigNumber = 2345; // Gia tri lon nhát cua long
+	long bigNumber = 2345; // Gia tri lon nhÃ¡t cua long
 	
 	// In ra cac gia tri
 	printf("Age: %d\n", age);
