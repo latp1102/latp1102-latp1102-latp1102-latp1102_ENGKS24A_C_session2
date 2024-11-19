435f25ef33b88f9cde6a19ed6123d05d69d8c984
latp1102 <latp1102@gmail.com>
2024-11-20 00:09:00 +0700

    Session01 - 3

---
+++ Session02-3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+	int num1 = 10;
+	int num2 = 5;
+	
+	// toan tu +
+	int total = num1 + num2;
+	printf(" total là %d\n", total);
+	
+	// toan tu -
+	int signal = num1 - num2;
+	printf(" total là %d\n", signal);
+	
+	// toan tu *
+	int core = num1 * num2;
+	printf(" core là %d\n", core);
+	
+	// toan tu divide and get the remainder %
+	int divide = num1 / num2;
+	printf("divide and get the remainder là %d\n", divide);
+	
+	return 0;
+}
